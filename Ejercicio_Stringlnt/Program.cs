8675d5b6eba2d16ad46ed01804f2f3b28e121c62
ortega.samuel@uabc.edu.mx <ortega.samuel@uabc.edu.mx>
2021-08-31 21:48:41 -0700

    Practica_2

---
+++ Ejercicio_Stringlnt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Stringlnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, edad;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            edad = Console.ReadLine();
+            Console.WriteLine("Te llamas " + nombre + "Y tienes:" + edad);
+            Console.WriteLine("Presiona Cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
