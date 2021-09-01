4cb00346b45fc82be17c54090b49c87e6fc1c5a7
ortega.samuel@uabc.edu.mx <ortega.samuel@uabc.edu.mx>
2021-08-31 19:27:30 -0700

    Practica_2

---
+++ CalculoPerimetro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10;
+            int B = 10;
+            int C = 7;
+            int p = A + B + C;
+            Console.WriteLine("El perimetro del triangulo es:" + p);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, ciudad;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe una ciudad");
+            ciudad = Console.ReadLine();
+            Console.WriteLine("Hola \n" + nombre + "bienvenido a" + ciudad);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+            
+        }
+    }
+}
